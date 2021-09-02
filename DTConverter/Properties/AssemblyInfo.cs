0634900018a67438b708f3994ab4d4cd96644305
Daniele <76773399+daniznf@users.noreply.github.com>
2021-09-02 23:33:39 +0200

    Add Available soon in Audio section

--- DTConverter/Properties/AssemblyInfo.cs
+++ DTConverter/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Windows;
 [assembly: NeutralResourcesLanguage("en")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.23.40.0")]
-[assembly: AssemblyFileVersion("0.23.40.0")]
+[assembly: AssemblyVersion("0.23.41.0")]
+[assembly: AssemblyFileVersion("0.23.41.0")]
