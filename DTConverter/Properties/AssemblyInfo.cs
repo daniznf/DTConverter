97cbea0298602f48e434d3084004fef83c243d22
Daniele <76773399+daniznf@users.noreply.github.com>
2021-09-06 17:53:27 +0200

    Change some margins in statusbar

--- DTConverter/Properties/AssemblyInfo.cs
+++ DTConverter/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Windows;
 [assembly: NeutralResourcesLanguage("en")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.25.39.0")]
-[assembly: AssemblyFileVersion("0.25.39.0")]
+[assembly: AssemblyVersion("0.25.40.0")]
+[assembly: AssemblyFileVersion("0.25.40.0")]
