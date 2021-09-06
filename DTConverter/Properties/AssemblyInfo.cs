8eb2e39a74d77325c0eac4d1523b23582eeafb50
Daniele <76773399+daniznf@users.noreply.github.com>
2021-09-06 18:44:19 +0200

    Move preview and time controls out of TabControl

--- DTConverter/Properties/AssemblyInfo.cs
+++ DTConverter/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Windows;
 [assembly: NeutralResourcesLanguage("en")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.25.47.0")]
-[assembly: AssemblyFileVersion("0.25.47.0")]
+[assembly: AssemblyVersion("0.25.48.0")]
+[assembly: AssemblyFileVersion("0.25.48.0")]
