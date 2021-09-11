3e9b77928fe8d772b8393792e62667e6edfec4cf
Daniele <76773399+daniznf@users.noreply.github.com>
2021-09-11 13:11:03 +0200

    Fix audio rate instead of sampling rate in status bar

--- DTConverter/Properties/AssemblyInfo.cs
+++ DTConverter/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Windows;
 [assembly: NeutralResourcesLanguage("en")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.30.35.0")]
-[assembly: AssemblyFileVersion("0.30.35.0")]
+[assembly: AssemblyVersion("0.30.36.0")]
+[assembly: AssemblyFileVersion("0.30.36.0")]
